79bf4109c9db1cab02c560a414613781b49d2838
Alan Weik <alaan.m@hotmail.com>
2020-11-09 16:18:14 +0100

    Entered in more querys, made minor adjustments to the database build. Might be done with the assignment. Included relational-schema.

--- Inlämningsuppgift 1.sql
+++ Inlämningsuppgift 1.sql
@@ -1,5 +1,15 @@
-DROP TABLE CUSTOMERS;
-CREATE TABLE CUSTOMERS (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, email_address TEXT);
+
+DROP TABLE IF EXISTS RENTALS;
+DROP TABLE IF EXISTS MOVIES;  
+DROP TABLE IF EXISTS STAFF_INFO;  
+DROP TABLE IF EXISTS CUSTOMERS;
+
+
+CREATE TABLE CUSTOMERS (
+customer_id INTEGER PRIMARY KEY, 
+first_name TEXT, 
+last_name TEXT, 
+email_address TEXT);
 
 --Denna kod stoppar in information (kunder) i vår nya tabell "customers". 
 INSERT INTO CUSTOMERS (first_name, last_name, email_address) VALUES ("Ricky", "Bobby", "assmaster@gmail.com");
@@ -13,8 +23,32 @@ INSERT INTO CUSTOMERS (first_name, last_name, email_address) VALUES ("Carlos", "
 INSERT INTO CUSTOMERS (first_name, last_name, email_address) VALUES ("Johhny", "Karate", "kentucky@gmail.com");
 INSERT INTO CUSTOMERS (first_name, last_name, email_address) VALUES ("Ashy", "Larry", "jesusss@gmail.com");
 
-DROP TABLE MOVIES;
-CREATE TABLE MOVIES (item_code INTEGER PRIMARY KEY, title TEXT, genre TEXT, director TEXT, studio TEXT);
+
+CREATE TABLE STAFF_INFO (
+title TEXT PRIMARY KEY, 
+cost INTEGER, 
+stock INTEGER
+);
+
+INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("Pulp Fiction", 29, 10);
+INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("Spirited Away", 29, 10);
+INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("Thunder Thighs", 10, 10);
+INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("Casa De Mi Padre", 10, 10);
+INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("Only God Forgives", 29, 10);
+INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("The Conjuring", 10, 10);
+INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("Berberian Sound Studio", 10, 10);
+
+
+CREATE TABLE MOVIES (
+item_code INTEGER PRIMARY KEY, 
+cost INTEGER,
+title TEXT, 
+genre TEXT, 
+director TEXT, 
+studio TEXT,
+FOREIGN KEY (title)
+       REFERENCES STAFF_INFO (title) 
+	   );
 
 --Denna kod stoppar in information (kunder) i vår nya tabell "customers". 
 INSERT INTO MOVIES (title, genre, director, studio) VALUES ("Pulp Fiction", "Action", "Quentin Tarantino", "Miramax");
@@ -25,28 +59,29 @@ INSERT INTO MOVIES (title, genre, director, studio) VALUES ("Only God Forgives",
 INSERT INTO MOVIES (title, genre, director, studio) VALUES ("The Conjuring", "Horror", "Gwen Stefani", "Glitter Studios");
 INSERT INTO MOVIES (title, genre, director, studio) VALUES ("Berberian Sound Studio", "Horror", "Silvio Berlusconi", "Pasta");
 
-DROP TABLE STAFF_INFO;
-CREATE TABLE STAFF_INFO (title TEXT PRIMARY KEY, cost INTEGER, stock INTEGER);
-
-INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("Pulp Fiction", 29, 10);
-INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("Spirited Away", 29, 10);
-INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("Thunder Thighs", 29, 10);
-INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("Casa De Mi Padre", 29, 10);
-INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("Only God Forgives", 29, 10);
-INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("The Conjuring", 29, 10);
-INSERT INTO STAFF_INFO (title, cost, stock) VALUES ("Berberian Sound Studio", 29, 10);
 
 
-DROP TABLE RENTALS; 
 CREATE TABLE RENTALS (
 rental_id INTEGER PRIMARY KEY, 
 customer_id INTEGER, 
-item_code INTEGER
---FOREIGN KEY (customer_id)
---       REFERENCES CUSTOMERS (customer_id)
---FOREIGN KEY (item_code)
---       REFERENCES MOVIES (item_code) 
+item_code INTEGER,
+FOREIGN KEY (customer_id)
+       REFERENCES CUSTOMERS (customer_id)
+FOREIGN KEY (item_code)
+       REFERENCES MOVIES (item_code) 
 	   );
+	
+--Denna kod skapar en uthyrningssimulering där kunder hyr olika titlar. 
+INSERT INTO RENTALS (customer_id, item_code) VALUES (4,6);
+INSERT INTO RENTALS (customer_id, item_code) VALUES (8,1);
+INSERT INTO RENTALS (customer_id, item_code) VALUES (4,4);
+INSERT INTO RENTALS (customer_id, item_code) VALUES (3,1);
+INSERT INTO RENTALS (customer_id, item_code) VALUES (7,2);
+INSERT INTO RENTALS (customer_id, item_code) VALUES (5,6);
+INSERT INTO RENTALS (customer_id, item_code) VALUES (1,2);
+INSERT INTO RENTALS (customer_id, item_code) VALUES (2,2);
+INSERT INTO RENTALS (customer_id, item_code) VALUES (3,7);
+INSERT INTO RENTALS (customer_id, item_code) VALUES (3,4);
 
 
 
--- Querys.sql
+++ Querys.sql
@@ -1,12 +1,52 @@
---Denna kod skapar en uthyrningssimulering där våra kunder hyr olika titlar
-INSERT INTO RENTALS (customer_id, item_code) VALUES (4,7);
-INSERT INTO RENTALS (customer_id, item_code) VALUES (8,1);
-INSERT INTO RENTALS (customer_id, item_code) VALUES (9,9);
-INSERT INTO RENTALS (customer_id, item_code) VALUES (3,2);
-INSERT INTO RENTALS (customer_id, item_code) VALUES (6,1);
-INSERT INTO RENTALS (customer_id, item_code) VALUES (7,9);
-INSERT INTO RENTALS (customer_id, item_code) VALUES (4,5);
-INSERT INTO RENTALS (customer_id, item_code) VALUES (5,2);
-INSERT INTO RENTALS (customer_id, item_code) VALUES (3,8);
-INSERT INTO RENTALS (customer_id, item_code) VALUES (4,4);
+--Denna kod skapar en retur-simlureing där våra kunder returnerar olika titlar. 
+DELETE FROM RENTALS 
+WHERE rental_id = 4;
+DELETE FROM RENTALS 
+WHERE rental_id = 9;
+DELETE FROM RENTALS 
+WHERE rental_id = 2;
+
+--Sortera filmer efter din favoritgenre:
+SELECT * FROM MOVIES
+WHERE genre LIKE "ACTION";
+
+--Sortera filmer efter din favoritregissör:
+SELECT director from MOVIES
+WHERE director LIKE "Quentin Tarantino";
+
+--Kolla vilka filmer som har rea:
+SELECT * FROM STAFF_INFO 
+WHERE cost < 20;
+
+--Visa hur många exemplar utav en specifik film som är uthyrda.
+SELECT * FROM RENTALS
+WHERE item_code LIKE 2;
+
+--Visa alla kunder som hyrde en viss film i kartläggning/reklamsyfte. 
+SELECT customer_id FROM RENTALS
+WHERE item_code LIKE 2;
+
+--Uppdaterar namn på en kolumn från cost till price. 
+ALTER TABLE STAFF_INFO
+RENAME COLUMN "cost" TO "price"; 
+
+ALTER TABLE STAFF_INFO
+RENAME COLUMN "stock" TO "inventory";
+
+--Uppdaterar namnet på en regissör. 
+UPDATE MOVIES 
+SET director = "James Wan"
+WHERE title LIKE "The Conjuring";
+
+UPDATE MOVIES 
+SET director = "Peter Strickland"
+WHERE title LIKE "Berberian Sound Studio";
+
+--Filtrera filmer efter pris (stigande):
+SELECT title, price
+FROM STAFF_INFO
+ORDER BY price;
+
+
+
 
